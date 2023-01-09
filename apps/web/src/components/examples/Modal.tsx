dd1f9263b01e9733483e34c3cfa54da5e8ed2691
Eddie SanMarco <esanmarco@linksquares.com>
2023-01-09 16:42:25 -0700

    removed button on triggerIcon

--- apps/web/src/components/examples/Modal.tsx
+++ apps/web/src/components/examples/Modal.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Button, Modal } from 'ui';
+import { Modal } from 'ui';
 
 export function ModalExample() {
     return (
@@ -8,8 +8,8 @@ export function ModalExample() {
             submit={() => console.log('submitted')}
             submitColor="info"
             triggerIcon={
-                <Button>
-                    Open Modal{' '}
+                <>
+                    <span className="mr-3">Open Modal</span>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -24,7 +24,7 @@ export function ModalExample() {
                             d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776"
                         />
                     </svg>
-                </Button>
+                </>
             }
         >
             <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
